98e9ee1c7b6b302aca9ea174e4bc3325543d5807
bouyahiasanae <bouyahiasanae.gmail.com>
2025-12-15 18:03:30 +0100

    refactor:updte name for a new apparence

---
+++ moi.js
@@ -0,0 +1,22 @@
+const texts = ["Développeur Web", "Spécialiste en Cyber Sécurité"];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+    if(count === texts.length){
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+    
+    document.querySelector('.typing-text').textContent = letter;
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+        setTimeout(type, 2000);
+    } else {
+        setTimeout(type, 150);
+    }
+})();
